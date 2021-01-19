5f48b90f84003f26facf35f08805b26fda708180
Chika <1609879250@qq.com>
2021-01-19 14:30:48 +0800

    SpringBootStudyProject @author Chika

---
+++ src/main/java/com/chikaho/config/WebMVConfig.java
@@ -0,0 +1,12 @@
+package com.chikaho.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+// 扩展SpringMVC
+@Configuration
+public class WebMVConfig implements WebMvcConfigurer {
+
+
+
+}
